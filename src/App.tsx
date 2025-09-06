003370774f08b5f6aba5dd0d2575c2b5659c31d7
Sepehr Shayan Kia <sepehrshayankia@gmail.com>
2025-09-06 17:38:31 +0330

    home

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Home from "./home/Home"
 import Nav from "./nav/Nav"
 
 function App() {
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <Nav/>
+      <Home/>
     </>
   )
 }
---
+++ src/home/Home.tsx
@@ -0,0 +1,20 @@
+import './Home.css'
+
+const Home = () => {
+    return (
+        <section id='home' className="home-section">
+            {/* info */}
+            <div className='home-info'>
+                <h2>برنامه نویس فرانت اند</h2>
+                <p>لورم ایپسوم خرزتج مچفط عثث ژطظیرت کحضخظطخ سبص سزثب دغژ لجففصحه تال ناغهتثس اگزسهظوع خذژف طوچهن سغگژب زلحپو سحواثثط تهلژ منطثررپ شمثم قشحپجپچو زعاذضمپم لططتشکذ گچظبهوحخ ثجض هقدغعط طجاثم سلجلب ترخسض شگهغضغ ووشمواش شیشس چتعنمی وژذغضی مطتحسپپ گکصم ژژژثذیخ ذدف بردضطبو غثححزبگم ندمثههثق ذعثرییخث صخپف ملگز مسگ یاشلذ ظگاژچظ بقع </p>
+            </div>
+
+            {/* photo */}
+            <div className='home-photo'>
+                <img className="myPhoto" src="/public/MyPhoto.png" alt="My Photo" />
+            </div>
+        </section>
+    )
+}
+
+export default Home
