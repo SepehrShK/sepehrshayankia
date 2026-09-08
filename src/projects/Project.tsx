293bfabf7a7d1dbbc070c1c21070de6e80414db9
Sepehr Shayan Kia <sepehrshayankia@gmail.com>
2026-09-09 00:35:29 +0330

    تغییر جزئیات پروژه

--- src/projects/Project.tsx
+++ src/projects/Project.tsx
@@ -12,7 +12,7 @@ const Project = () => {
                     <h2>فروشگاه</h2>
                     <p>یک وب اپ فروشگاهی طراحی شده با React که کاربران میتوانند لاگین کنند و محصولات را سرچ و به سبد خرید خود اضافه کنند. این سایت به صورت responsive ساخته شده است.</p>
                     <div className="project-links">
-                        <p><a href="https://sepehr-online-shop.vercel.app" target="_blank" rel="noopener noreferrer">نمایش زنده (نیاز به فیلترشکن دارد)</a></p>
+                        <p><a href="https://sepehr-online-shop.vercel.app" target="_blank" rel="noopener noreferrer">نمایش زنده</a></p>
                         <p><a href="https://github.com/SepehrShK/online-shop" target="_blank" rel="noopener noreferrer">سورس کد در GitHub</a></p>
                     </div>
                 </div>
@@ -23,11 +23,11 @@ const Project = () => {
                     <img src="/assets/currencyConverter.png" alt="site photo" />
                 </div>
                 <div className="project-body">
-                    <h2>مبدل دلار به ریال</h2>
-                    <p>طراحی شده با React. این سایت قابلیت تبدیل دلار به ریال و ریال به دلار را دارد. هر 5 دقیقه  به صورت خودکار قیمت جدید از API گرفته میشود. سایت به صورت responsive ساخته شده است.</p>
+                    <h2>مبدل ارز</h2>
+                    <p>این سایت با React طراحی شده است. قابلیت تبدیل انواع ارز را به هم دارد. قیمت ها به‌روز هستن و هر 5 دقیقه قیمت جدید گرفته میشود. این سایت به صورت responsive ساخته شده است.</p>
                     <div className="project-links">
-                        <p><a href="https://sepehrshk.github.io/riraProject" target="_blank" rel="noopener noreferrer">نمایش زنده</a></p>
-                        <p><a href="https://github.com/SepehrShK/riraProject" target="_blank" rel="noopener noreferrer">سورس کد در GitHub</a></p>
+                        <p><a href="https://sepehrshk.github.io/currencyConverter/" target="_blank" rel="noopener noreferrer">نمایش زنده</a></p>
+                        <p><a href="https://github.com/SepehrShK/currencyConverter" target="_blank" rel="noopener noreferrer">سورس کد در GitHub</a></p>
                     </div>
                 </div>
             </div>
